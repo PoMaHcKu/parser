0a0b1902fea1ec37a906ec0d2aa645c905523170
r.popov <rom2517pop@gmail.com>
2020-10-20 17:41:45 +0300

    Created url validation during typing url.

---
+++ src/main/java/com/task/parser/validator/JsfValidator.java
@@ -0,0 +1,30 @@
+package com.task.parser.validator;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("validator")
+public class JsfValidator implements Validator<String> {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, String s) throws ValidatorException {
+        UrlValidator urlValidator = new UrlValidator();
+        s = checkAndGetHttpProtocol(s);
+        if (!urlValidator.isValid(s)) {
+            throw new ValidatorException(new FacesMessage(s + " incorrect url"));
+        }
+    }
+
+    public String checkAndGetHttpProtocol(String url) {
+        if (url.startsWith("https://") || url.startsWith("http://")) {
+            return url;
+        }
+        return "http://" + url;
+    }
+}
