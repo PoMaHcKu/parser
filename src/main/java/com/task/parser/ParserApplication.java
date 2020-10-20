12ac0ba925e7dfa76b5c5acb9d66d7abf960739a
r.popov <rom2517pop@gmail.com>
2020-10-20 20:38:16 +0300

    Migration to war file is in process.

--- src/main/java/com/task/parser/ParserApplication.java
+++ src/main/java/com/task/parser/ParserApplication.java
@@ -2,10 +2,22 @@ package com.task.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
-public class ParserApplication {
+public class ParserApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ParserApplication.class, args);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setInitParameter(
+                "com.sun.faces.expressionFactory",
+                "org.apache.el.ExpressionFactoryImpl"
+        );
     }
 }
