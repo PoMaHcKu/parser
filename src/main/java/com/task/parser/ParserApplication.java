22201062218a9dc1a7f747a71c0b425194240481
pomahcku <rom2517pop@gmail.com>
2020-10-21 11:19:49 +0300

    Moved servlet configs to web.xml

--- src/main/java/com/task/parser/ParserApplication.java
+++ src/main/java/com/task/parser/ParserApplication.java
@@ -2,21 +2,10 @@ package com.task.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import javax.faces.webapp.FacesServlet;
 
 @SpringBootApplication
 public class ParserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParserApplication.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        return (ServletRegistrationBean<FacesServlet>)
-                new ServletRegistrationBean(servlet, "*.xhtml");
     }
 }
